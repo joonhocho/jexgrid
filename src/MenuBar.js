88c5f43d6c91d8a51d9017802cfdd12e8659c74f
JoonHo Cho <joonho1101@gmail.com>
2011-10-12 17:41:03 +0900

    a

--- src/MenuBar.js
+++ src/MenuBar.js
@@ -107,7 +107,7 @@ proto._init = function(args) {
 	}
 	var ul = this.ul = $(element('ul', {
 		'class': 'jgrid-column-toggle-box'
-	}, list, SAFE)).appendTo(document.body);
+	}, list, SAFE)).appendTo(this.grid._ctnr);
 
 	var offset = ul.offset();
 	ul.css({
